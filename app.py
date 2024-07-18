8085e48928f0dd4e1616c3390087e779898814fa
Vineet Kumar <gitonde220sanjaysgnr@gmail.com>
2024-07-18 22:57:57 +0530

    house price predictor

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from flask import Flask, render_template, request
 app = Flask(__name__)
@@ -20,4 +21,6 @@ def home():
 
     return render_template('index.html', result = price)
 
-app.run(debug=True)
+#app.run(debug=True)
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
